75ad529cf45c38687a47c8cb4e69d6b0ea38d77f
aott20 <adderotter@hotmail.com>
2018-10-17 18:39:18 -0600

    Final prep

--- homerun.r
+++ homerun.r
@@ -4,7 +4,11 @@ Houston <- c(8,13,13,15,31,16,10,17,25)
 
 Boston <- c(5,15,10,23,21,16,13,32,43)
 
-#Bootstrap to create a ci the difference in means
+#My friend bet me dinner that Houston would hit more HR than Boston in this series. I'm
+#willing to take this bet if I'm 80% confident that the average Red Sox starter hit more 
+#HR than the average Houston starter over the course of the season. Use the bootstrap procedure
+#to form an 80% ci on the mean difference in HR's for starters of the two teams. From the 
+#ci, should I take the bet?
 
 mean(Houston) - mean(Boston)
 
@@ -14,12 +18,15 @@ hr_dist <- replicate(nreps, mean(sample(Houston, replace = TRUE)) - mean(sample(
 
 quantile(hr_dist, c(.1,.9))
 
+#I should not take the bet.
+
 #The strength of the team is the team, meaning you don't want a big difference between your biggest
 #hr hitter and your worst hr hitter. A test statistic for this is
-#hr = (max HR (team 1) - min HR (team 1)) / ((max HR (team 2) - min HR (team 2)) 
+#hr = (max HR (team 1) - min HR (team 1)) / ((max HR (team 2) - min HR (team 2))
+#Use a permutation test to see if this test statistic is abnormal (greater than 5% of the
+#distribution)
 
 test <- (max(Boston)- min(Boston)) / (max(Houston) - min(Houston))
-
 
 all <- c(Boston, Houston)
 dist <- numeric(nreps)
@@ -36,3 +43,5 @@ est <- mean(dist >= test)
 ci <- est + c(-1,1)*1.96*sqrt(est*(1-est)/nreps)
 
 c(Estimate = est, Lower = ci[1], Upper = ci[2])
+
+#Based on this p value, I cannot conclude that the test statistic for this series is exceptional.
