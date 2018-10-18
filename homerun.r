45bc4daffa505fb23cd10886c3b9b6cdc62042a7
aott20 <adderotter@hotmail.com>
2018-10-17 18:06:55 -0600

    Final Commit

--- homerun.r
+++ homerun.r
@@ -4,14 +4,35 @@ Houston <- c(8,13,13,15,31,16,10,17,25)
 
 Boston <- c(5,15,10,23,21,16,13,32,43)
 
-#Bootstrap (like the roommate vs you shower time) the difference in means
+#Bootstrap to create a ci the difference in means
 
 mean(Houston) - mean(Boston)
 
+nreps <- 10000
 
+hr_dist <- replicate(nreps, mean(sample(Houston, replace = TRUE)) - mean(sample(Boston, replace = TRUE)))
+
+quantile(hr_dist, c(.1,.9))
 
 #The strength of the team is the team, meaning you don't want a big difference between your biggest
 #hr hitter and your worst hr hitter. A test statistic for this is
 #hr = (max HR (team 1) - min HR (team 1)) / ((max HR (team 2) - min HR (team 2)) 
 
-(max(Boston)- min(Boston)) / (max(Houston) - min(Houston))
+test <- (max(Boston)- min(Boston)) / (max(Houston) - min(Houston))
+
+
+all <- c(Boston, Houston)
+dist <- numeric(nreps)
+
+for (i in 1:nreps){
+  x <- sample(1:18, 9, replace = FALSE)
+  new1 <- all[x]
+  new2 <- all[-x]
+  dist[i] <- (max(new1)- min(new1)) / (max(new2) - min(new2))
+}
+
+est <- mean(dist >= test)
+
+ci <- est + c(-1,1)*1.96*sqrt(est*(1-est)/nreps)
+
+c(Estimate = est, Lower = ci[1], Upper = ci[2])
--- mpg.r
+++ mpg.r
@@ -4,7 +4,36 @@ data(mtcars)
 
 #Select only the mpg and am vectors
 
-cars <- mtcars[,c(1,9)]
+car <- mtcars[,c(1,9)]
+head(car)
+dim(car)
 
-#Use permutation test to see if there is a signifcant difference in mpg between automatic
-#and manual cars
+# Set number of repetitions
+nreps <- 10000
+
+### PERMUTATION ###
+# Is miles per gallon better for manual cars than for automatic cars? (0 automatic, 1 manual)
+auto <- car[car$am == 0,1]
+manual <- car[car$am == 1,1]
+obs_diff <- mean(manual) - mean(auto)
+
+car_perm <- car
+diff <- replicate(nreps, {
+  car_perm$am <- sample(0:1, dim(car_perm)[1], replace = TRUE)
+  auto_new <- car_perm[car_perm$am == 0,1]
+  manual_new <- car_perm[car_perm$am == 1,1]
+  mean(manual_new) - mean(auto_new)
+})
+c("p value" = mean(diff >= obs_diff))
+# Yes, manual cars have better miles per gallon than automatic cars
+
+
+
+### BOOTSTRAP ###
+# What is a 95% confidence interval for the mean miles per gallon of automatic cars?
+auto <- car[car$am == 0,1]
+
+vec <- replicate(nreps, mean(sample(auto, replace = TRUE)))
+
+quantile(vec, c(.025, .975))
+mean(auto)
